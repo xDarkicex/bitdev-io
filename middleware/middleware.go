6709d2a1625742138b1e9693246a11bc8cda4433
Gentry Rolofson <xdarkicex@gmail.com>
2020-02-28 09:34:54 -0800

    whats up

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,46 +1,64 @@
 package middleware
 
+import (
+		"github.com/labstack/echo/v4"
+		"github.com/labstack/echo/v4/middleware"
+
+)
+
 func init() {
 
 
 }
 
 type Config struct {
-		// Skipper defines a function to skip middleware.
+		ContentSecurityPolicy string `yaml:"content_security_policy"`
+		StrictTransfer string
 		// default middleware.DefaultSkipper
 		Skipper middleware.Skipper
-
-		// WhiteList is an allowed ip list.
-		WhiteList []string
-
-		// BlackList is a disallowed ip list.
-		BlackList []string
-
-		// Block by default.
-		BlockByDefault bool
+		ContentTypeNosniff string `yaml:"content_type_nosniff"`
+		XSSProtection string `yaml:"xss_protection"`
 }
 
 // SecureDefaultConfig is the default SecureHeader middleware config
 var SecureDefaultConfig = Config{
 		Skipper:        middleware.DefaultSkipper,
-		BlockByDefault: false,
+		ContentTypeNosniff: "nosniff",
+		XSSProtection: "1; mode=block",
+		ContentSecurityPolicy:  "block-all-mixed-content",
+		StrictTransfer: "max-age=31536000; includeSubDomains",
 }
 
+// Secure header middleware
 func SecureHeader() echo.MiddlewareFunc {
 		return secureHeaderWithConfig(SecureDefaultConfig)
 }
 
 func secureHeaderWithConfig(config Config) echo.MiddlewareFunc {
-		var err error
 		if config.Skipper == nil {
-				config.Skipper = DefaultConfig.Skipper
+				config.Skipper = SecureDefaultConfig.Skipper
+		}
+		if config.ContentTypeNosniff == "" {
+				config.ContentTypeNosniff = SecureDefaultConfig.ContentTypeNosniff
+		}
+		if config.XSSProtection == "" {
+				config.XSSProtection = SecureDefaultConfig.XSSProtection
+		}
+		if config.ContentSecurityPolicy == "" {
+				config.ContentSecurityPolicy = SecureDefaultConfig.ContentSecurityPolicy
+		}
+		if config.StrictTransfer == "" {
+				config.StrictTransfer = SecureDefaultConfig.StrictTransfer
 		}
 		return func(next echo.HandlerFunc) echo.HandlerFunc {
 				return func(c echo.Context) error {
 						if config.Skipper(c) {
 								return next(c)
 						}
-
+						c.Response().Header().Set(echo.HeaderStrictTransportSecurity, config.StrictTransfer)
+						c.Response().Header().Set(echo.HeaderContentSecurityPolicy, config.ContentSecurityPolicy)
+						c.Response().Header().Set(echo.HeaderXXSSProtection, config.XSSProtection)
+						c.Response().Header().Set(echo.HeaderXContentTypeOptions, config.ContentTypeNosniff)
 						return next(c)
 				}
 		}
